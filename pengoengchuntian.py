690168e085a46771ab393d1a3ccd0e5f8f997668
zhanghoumeng <38311999+zhanghoumeng@users.noreply.github.com>
2018-04-17 14:44:04 +0800

    Update pengoengchuntian.py

--- pengoengchuntian.py
+++ pengoengchuntian.py
@@ -3,11 +3,11 @@
 L = [11,12,3,13,19,45,23,14]
 
 
-def Sort_1(L):
-    for i in range(len(L)-1):
-        for j in range (i, len(L)-1):
-            if L[i] > L[j+1]:
-                L[i],L[j + 1] = L[j + 1], L[i]
-    return L
-a = Sort_1(L)
-print(a)
+def maopao (list)
+    for out in range(len(list)-1,0,-1)
+        for inner in range(0,out)
+            if list[inner] > list[inner + 1]
+            temp = list[inner]
+            list[inner + 1] = list[inner]
+            list[inner] = temp
+        print(list)
